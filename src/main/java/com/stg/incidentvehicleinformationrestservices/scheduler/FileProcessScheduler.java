446417fab0dd25addc4dfa9bee9f56384f41e783
chinna vadada <chinnachalam@gmail.com>
2023-09-27 09:42:33 +0530

    added sync

--- src/main/java/com/stg/incidentvehicleinformationrestservices/scheduler/FileProcessScheduler.java
+++ src/main/java/com/stg/incidentvehicleinformationrestservices/scheduler/FileProcessScheduler.java
@@ -11,11 +11,8 @@ import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.StreamSupport;
 
 @Component
 public class FileProcessScheduler {
@@ -27,41 +24,41 @@ public class FileProcessScheduler {
 
     @Scheduled(cron = "0 0/5 * * * ?")
     public void processFile() {
-        System.out.println("**** STARTED PROCESS FILE *****");
+        synchronized (this) {
+            System.out.println("**** STARTED PROCESS FILE *****");
 
-        // read from todo --> in_process
+            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream(FILE_NAME)));
+                 CSVParser csvParser = new CSVParser(bufferedReader,
+                     CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim());) {
+                Set<Incident> incidents = new HashSet<>();
+                Iterable<CSVRecord> csvRecords = csvParser.getRecords();
 
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream(FILE_NAME)));
-             CSVParser csvParser = new CSVParser(bufferedReader,
-                 CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim());) {
-            Set<Incident> incidents = new HashSet<>();
-            Iterable<CSVRecord> csvRecords = csvParser.getRecords();
+                for (CSVRecord csvRecord : csvRecords) {
+                    Incident incident = new Incident();
+                    incident.setLogNumber(csvRecord.get("LOG_NO"));
+                    incident.setDcPrefix(csvRecord.get("DC_PREFIX"));
+                    incident.setDcName(csvRecord.get("DC_NAME"));
+                    incident.setDcFault(csvRecord.get("DC_FAULT"));
+                    incident.setDcAvail(csvRecord.get("DC_AVAIL"));
+                    incident.setLocFrnt(csvRecord.get("LOC_FRNT"));
+                    incident.setLocRear(csvRecord.get("LOC_REAR"));
+                    incident.setLocLeft(csvRecord.get("LOC_LEFT"));
+                    incident.setLocRigh(csvRecord.get("LOC_RIGH"));
+                    incident.setPartRec(csvRecord.get("PART_REC"));
+                    incident.setPartmemo(csvRecord.get("PARTMEMO"));
+                    incident.setCodedBy(csvRecord.get("CODED_BY"));
+                    incident.setFieldInv(csvRecord.get("FIELD_INV"));
+                    incidents.add(incident);
+                }
 
-            for (CSVRecord csvRecord : csvRecords) {
-                Incident incident = new Incident();
-                incident.setLogNumber(csvRecord.get("LOG_NO"));
-                incident.setDcPrefix(csvRecord.get("DC_PREFIX"));
-                incident.setDcName(csvRecord.get("DC_NAME"));
-                incident.setDcFault(csvRecord.get("DC_FAULT"));
-                incident.setDcAvail(csvRecord.get("DC_AVAIL"));
-                incident.setLocFrnt(csvRecord.get("LOC_FRNT"));
-                incident.setLocRear(csvRecord.get("LOC_REAR"));
-                incident.setLocLeft(csvRecord.get("LOC_LEFT"));
-                incident.setLocRigh(csvRecord.get("LOC_RIGH"));
-                incident.setPartRec(csvRecord.get("PART_REC"));
-                incident.setPartmemo(csvRecord.get("PARTMEMO"));
-                incident.setCodedBy(csvRecord.get("CODED_BY"));
-                incident.setFieldInv(csvRecord.get("FIELD_INV"));
-                incidents.add(incident);
+                incidents.forEach(incident -> incidentRepository.save(incident)); // use spring-batch to insert all records.
+
+                System.out.println("**** COMPLETED PROCESS FILE *****");
+
+                // move file to completed
+            } catch (Exception e) {
+                System.out.println("fail to parse CSV file: " + e.getMessage());
             }
-
-            incidents.forEach(incident -> incidentRepository.save(incident)); // use spring-batch to insert all records.
-
-            System.out.println("**** COMPLETED PROCESS FILE *****");
-
-            // move file to completed
-        } catch (Exception e) {
-            System.out.println("fail to parse CSV file: " + e.getMessage());
         }
     }
 }
